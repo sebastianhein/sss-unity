f2ed159cb1a2a1f686fb4e58d6103fff0b4f9ad7
Sebastian Hein <sebastian.hein@artcom.de>
2015-05-01 15:23:16 +0200

    Lucy Demo and Initial shader Version

---
+++ Assets/UpdateShader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdateShader : MonoBehaviour {
+
+    public Light _light;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = _light.transform.position;
+   
+
+        GetComponent<MeshRenderer>().sharedMaterial.SetVector("_lightPosition", new Vector4(pos.x, pos.y, pos.z, 0));
+
+	}
+}
